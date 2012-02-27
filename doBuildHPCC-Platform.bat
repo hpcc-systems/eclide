ae25ae3adc30c94a29b0eb91e9bb6a9d93c7636e
Gordon Smith <gordon.smith@lexisnexis.com>
2012-02-27 16:15:27 +0000

    Cleanup build (for new build machine)
    External Libraries changed to newer / unmodified variants.
    
    Signed-off-by: Gordon Smith <gordon.smith@lexisnexis.com>

---
+++ doBuildHPCC-Platform.bat
@@ -0,0 +1,19 @@
+@echo off
+call "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcvarsall.bat" x86
+
+set ROOT_DIR=D:\hpccsystems
+set SRC_DIR=%ROOT_DIR%\src
+set BUILD_DIR=%SRC_DIR%\build
+set EXTERNALS_DIR=%ROOT_DIR%\externals
+echo %SRC_DIR%
+echo %BUILD_DIR%
+echo %EXTERNALS_DIR%
+
+md %BUILD_DIR%\HPCC-Platform
+cd %BUILD_DIR%\HPCC-Platform
+
+cmake -G"Visual Studio 9 2008" -DEXTERNALS_DIRECTORY="%EXTERNALS_DIR%" "%SRC_DIR%/HPCC-Platform"
+
+msbuild /p:Configuration=Release hpccsystems-platform.sln
+
+cd ..\..
---
+++ doBuildLN.bat
@@ -0,0 +1,19 @@
+@echo off
+call "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcvarsall.bat" x86
+
+set ROOT_DIR=D:\hpccsystems
+set SRC_DIR=%ROOT_DIR%\src
+set BUILD_DIR=%SRC_DIR%\build
+set EXTERNALS_DIR=%ROOT_DIR%\externals
+echo %SRC_DIR%
+echo %BUILD_DIR%
+echo %EXTERNALS_DIR%
+
+md %BUILD_DIR%\LN
+cd %BUILD_DIR%\LN
+
+cmake -G"Visual Studio 9 2008" -DEXTERNALS_DIRECTORY="%EXTERNALS_DIR%" "%SRC_DIR%/LN"
+
+msbuild /p:Configuration=Release lexisnexisrs-platform.sln
+
+cd ..\..
