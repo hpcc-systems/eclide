84c1a4d920d37ca3811a8f0da9e730ea7193ed5a
Gordon Smith <gordon.smith@lexisnexis.com>
2013-01-18 11:51:45 +0000

    IDE-221 MSI Internal Version Number Issue
    The build number is not being taken into consideration during the upgrade check.
    
    Fixes IDE-221
    
    Signed-off-by: Gordon Smith <gordon.smith@lexisnexis.com>

---
+++ version.cmake
@@ -0,0 +1,10 @@
+###
+## Version Information
+###
+set ( HPCC_PROJECT "community" )
+set ( HPCC_MAJOR 6 )
+set ( HPCC_MINOR 10 )
+set ( HPCC_POINT 1 )
+set ( HPCC_MATURITY "rc" )
+set ( HPCC_SEQUENCE 0 )
+###
