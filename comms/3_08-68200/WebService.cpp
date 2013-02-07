9b54e92fe2340afdffbb0b7a26afea830b5e128b
Gordon Smith <gordon.smith@lexisnexis.com>
2013-02-07 11:54:23 +0000

    IDE-234 Unable to resolve queue names for 702 servers
    Fixes IDE-234
    
    Signed-off-by: Gordon Smith <gordon.smith@lexisnexis.com>

---
+++ comms/3_08-68200/WebService.cpp
@@ -0,0 +1,9 @@
+#include "soapC.cpp"
+
+#include "soapws_USCOREaccountServiceSoapProxy.cpp"
+#include "soapWsAttributesServiceSoapProxy.cpp"
+#include "soapWsDfuServiceSoapProxy.cpp"
+#include "soapWsSMCServiceSoapProxy.cpp"
+#include "soapWsTopologyServiceSoapProxy.cpp"
+#include "soapWsWorkunitsServiceSoapProxy.cpp"
+
