5f65d5cb6d3b279f38bc541e91d58ef9f967ddea
Richard Chapman <rchapman@hpccsystems.com>
2013-03-27 18:31:05 +0000

    Split of closedown-4.0.x
    Signed-off-by: Richard Chapman <rchapman@hpccsystems.com>

--- version.cmake
+++ version.cmake
@@ -2,10 +2,10 @@
 ## Version Information
 ###
 set ( HPCC_PROJECT "community" )
-set ( HPCC_MAJOR 6 )
-set ( HPCC_MINOR 11 )
-set ( HPCC_POINT 1 )
-set ( HPCC_MATURITY "trunk" )
+set ( HPCC_MAJOR 4 )
+set ( HPCC_MINOR 0 )
+set ( HPCC_POINT 0 )
+set ( HPCC_MATURITY "closedown" )
 set ( HPCC_SEQUENCE 0 )
 ###
 IF ( ${HPCC_POINT} GREATER 65 )
