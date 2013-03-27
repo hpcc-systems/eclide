5f2889dfdc82887b8819e7d26d30ce09e7efa9c9
Richard Chapman <rchapman@hpccsystems.com>
2013-03-27 18:33:38 +0000

    Split off closedown-4.0.x
    Signed-off-by: Richard Chapman <rchapman@hpccsystems.com>

--- version.cmake
+++ version.cmake
@@ -2,9 +2,9 @@
 ## Version Information
 ###
 set ( HPCC_PROJECT "community" )
-set ( HPCC_MAJOR 6 )
-set ( HPCC_MINOR 11 )
-set ( HPCC_POINT 1 )
+set ( HPCC_MAJOR 4 )
+set ( HPCC_MINOR 1 )
+set ( HPCC_POINT 0 )
 set ( HPCC_MATURITY "trunk" )
 set ( HPCC_SEQUENCE 0 )
 ###
