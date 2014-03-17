dc466bea51ea8aa022b82e16beb61dba09cbe12a
Richard Chapman <rchapman@hpccsystems.com>
2014-03-17 09:24:24 +0000

    Split off candidate-4.2.4 branch
    Signed-off-by: Richard Chapman <rchapman@hpccsystems.com>

--- version.cmake
+++ version.cmake
@@ -4,8 +4,8 @@
 set ( HPCC_PROJECT "community" )
 set ( HPCC_MAJOR 4 )
 set ( HPCC_MINOR 2 )
-set ( HPCC_POINT 3 )
-set ( HPCC_MATURITY "closedown" )
+set ( HPCC_POINT 4 )
+set ( HPCC_MATURITY "rc" )
 set ( HPCC_SEQUENCE 0 )
 ###
 IF ( ${HPCC_POINT} GREATER 65 )
