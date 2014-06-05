50e96bee601a010a05ac57d106e0d720211bab87
Gordon Smith <gordon.smith@lexisnexis.com>
2014-06-05 12:51:07 +0100

    IDE-360 Add support for WTL 9.0
    Fixes IDE-360
    
    Signed-off-by: Gordon Smith <gordon.smith@lexisnexis.com>

--- wlib/SplitterWnd.h
+++ wlib/SplitterWnd.h
@@ -1,10 +1,22 @@
 #pragma once
 
 template<bool t_bVertical = true>
-class CMySplitterWindow : public WTL::CSplitterWindowImpl<CMySplitterWindow<t_bVertical>, t_bVertical>
+class CMySplitterWindow : 
+#if _WTL_VER < 0x0900
+	public WTL::CSplitterWindowImpl<CMySplitterWindow<t_bVertical>, t_bVertical>
+#else
+	public WTL::CSplitterWindowImpl<CMySplitterWindow<t_bVertical> >
+#endif
 {
 public:
 	DECLARE_WND_CLASS_EX(_T("WTL_MySplitterWindow"), CS_DBLCLKS, COLOR_WINDOW)
+
+#if _WTL_VER < 0x0900
+#else
+	CMySplitterWindow() : CSplitterWindowImpl<CMySplitterWindow<t_bVertical> >(t_bVertical)
+	{ 
+	}
+#endif
 
 	void DrawSplitterBar(WTL::CDCHandle dc)
 	{
