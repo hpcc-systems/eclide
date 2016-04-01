7ba3164788a18bea1ec63106b450fb8f564e377c
Richard Chapman <rchapman@hpccsystems.com>
2016-04-01 16:48:46 +0100

    ECL IDE 6.0.0-rc1 Release Candidate 1
    Signed-off-by: Richard Chapman <rchapman@hpccsystems.com>

--- version.cmake
+++ version.cmake
@@ -5,7 +5,7 @@ set ( HPCC_PROJECT "community" )
 set ( HPCC_MAJOR 6 )
 set ( HPCC_MINOR 0 )
 set ( HPCC_POINT 0 )
-set ( HPCC_MATURITY "beta" )
+set ( HPCC_MATURITY "rc" )
 set ( HPCC_SEQUENCE 1)
 ###
 IF ( ${HPCC_POINT} GREATER 65 )
