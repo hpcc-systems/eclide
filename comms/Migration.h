d53e4390bc8c5a86243865f3f06dd807ed264011
Gordon Smith <gordonjsmith@gmail.com>
2016-09-05 17:16:42 +0100

    IDE-538 Import large MOD files, fail
    * Adding support for files in the 300->600MB range.
    * Keep ECL in "narrow" strings for as long as possible.
    * Judicious clearing of memory structes as the core content is passed around.
    * Eliminate string copies where possible.
    * Stream from file.
    
    Fixes IDE-538
    
    Signed-off-by: Gordon Smith <gordonjsmith@gmail.com>

--- comms/Migration.h
+++ comms/Migration.h
@@ -5,6 +5,20 @@
 #include "Workspace.h"
 #include "Thread.h"
 
+__interface IMigrationItem : public IUnknown
+{
+    void QualifiedLabel(const std::_tstring & modAttrLabel);
+    std::_tstring QualifiedLabel() const;
+    std::_tstring ModuleLabel() const;
+    std::_tstring AttributeLabel() const;
+    IAttributeType * AttributeType() const;
+
+    bool HasComment() const;
+    void Comment(const std::_tstring & comment);
+    std::_tstring Comment() const;
+    std::_tstring ECL() const;
+};
+
 __interface IMigrationCallback
 {
     BOOL Invalidate(BOOL bErase = TRUE);
@@ -19,6 +33,7 @@ __interface IMigration : public IUnknown
     void AddToRep(IModuleAdapt targetModule, IAttributeHistoryAdapt fromAttr, const std::_tstring & comment, bool sandbox);
     void AddEclToRep(const std::_tstring & modLabel, const std::_tstring & attrLabel, IAttributeType * type, const std::_tstring & comment, const std::_tstring & ecl, const std::_tstring & by, bool sandbox);
     void AddEclToRep(const std::_tstring & modAttrLabel, IAttributeType * type, const std::_tstring & comment, const std::_tstring & ecl, const std::_tstring & by, bool sandbox);
+    void AddEclToModule(IModuleAdapt module, StlLinked<IMigrationItem> migrationItem, const std::_tstring & by, bool sandbox);
     void AddEclToModule(IModuleAdapt module, const std::_tstring & modLabel, const std::_tstring & attrLabel, IAttributeType * type, const std::_tstring & comment, const std::_tstring & ecl, const std::_tstring & by, bool sandbox);
     void AddEclToModule(IModuleAdapt module, const std::_tstring & modAttrLabel, IAttributeType * type, const std::_tstring & comment, const std::_tstring & ecl, const std::_tstring & by, bool sandbox);
     void Start();
@@ -37,3 +52,4 @@ __interface IMigration : public IUnknown
 //		::Sleep(1000);
 
 COMMS_API IMigration * CreateIMigration(IRepository * targetRep, bool setTextNoBroadcast, IMigrationCallback * caller = NULL);
+COMMS_API IMigrationItem * CreateIMigrationItem(const std::_tstring & modAttrLabel, IAttributeType * attrType, const std::string & comment, const std::string & ecl);
