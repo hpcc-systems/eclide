218ade8ba79170bea743535ac887cb444cb95071
dehilsterlexis <david.dehilster@lexisnexis.com>
2017-01-04 15:00:34 -0500

    IDE-591 Colorize and iconize different file types in the repository listing
    Fixed IDE-591
    
    Signed-off-by: dehilsterlexis <david.dehilster@lexisnexis.com>

--- wlib/RepositoryTreeNode.cpp
+++ wlib/RepositoryTreeNode.cpp
@@ -275,8 +275,8 @@ CAttributeNode::~CAttributeNode()
 void CAttributeNode::GenerateDispText()
 {
 	m_DispText = m_displayQualified ? m_attribute->GetQualifiedLabel() : m_attribute->GetLabel();
-    m_DispText += m_attribute->GetType()->GetFileExtension();
-    std::_tstring lockedBy = m_attribute->GetLockedBy();
+	m_DispText += m_attribute->GetType()->GetFileExtension();
+	std::_tstring lockedBy = m_attribute->GetLockedBy();
 	if (lockedBy.length())
 	{
 		m_DispText += _T(" (");
