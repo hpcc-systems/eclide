69a1bb302f46a7c926c9682c73757d060894271f
Gordon Smith <gordonjsmith@gmail.com>
2018-08-04 07:43:46 +0100

    IDE-787 Bump comms layer to 6.4.0
    Signed-off-by: Gordon Smith <gordonjsmith@gmail.com>

---
+++ comms/6_4_0/gsoap_build.bat
@@ -0,0 +1,10 @@
+set GSOAP=..\..\..\gsoap-2.7\gsoap
+set GSOAP_BIN=%GSOAP%\bin\win32
+set wsdl2h=%GSOAP_BIN%\wsdl2h.exe -I%GSOAP% -I.\hpcc
+set soapcpp2=%GSOAP_BIN%\soapcpp2.exe -C -x -i -I%GSOAP%\import
+
+%wsdl2h% -o Services.h Ws_Account.wsdl WsAttributes.wsdl WsDfu.wsdl WsSMC.wsdl WsTopology.wsdl WsWorkunits.wsdl
+
+%soapcpp2% Services.h
+
+%soapcpp2% -penv .\hpcc\env.h
