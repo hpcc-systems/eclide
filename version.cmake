4c5cb272bcaf4b5478816b4799d56a9419388bf7
Richard Chapman <rchapman@hpccsystems.com>
2018-10-19 15:02:18 +0100

    Split off 7.2.0
    Signed-off-by: Richard Chapman <rchapman@hpccsystems.com>

--- version.cmake
+++ version.cmake
@@ -5,10 +5,10 @@ set ( HPCC_PROJECT "community" )
 set ( HPCC_MAJOR 7 )
 
 
-set ( HPCC_MINOR 0 )
+set ( HPCC_MINOR 2 )
 set ( HPCC_POINT 0 )
-set ( HPCC_MATURITY "rc" )
-set ( HPCC_SEQUENCE 3 )
+set ( HPCC_MATURITY "closedown" )
+set ( HPCC_SEQUENCE 0 )
 ###
 IF ( ${HPCC_POINT} GREATER 65 )
   error ( "HPCC_POINT must be less than 66." )
