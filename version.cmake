c5ae1d4789c30281b2a549b0771a700b638707e3
Richard Chapman <rchapman@hpccsystems.com>
2019-03-14 17:20:09 +0000

    Split off 7.0.22
    Signed-off-by: Richard Chapman <rchapman@hpccsystems.com>

--- version.cmake
+++ version.cmake
@@ -1,12 +1,13 @@
 ###
 ## Version Information
 ###
+set ( HPCC_NAME "ECL IDE" )
 set ( HPCC_PROJECT "community" )
 set ( HPCC_MAJOR 7 )
 
 
 set ( HPCC_MINOR 0 )
-set ( HPCC_POINT 7 )
+set ( HPCC_POINT 23 )
 set ( HPCC_MATURITY "closedown" )
 set ( HPCC_SEQUENCE 0 )
 ###
