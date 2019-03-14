a1ce53b0effb4948a3136ba3df2a30f5d4dd5edd
Richard Chapman <rchapman@hpccsystems.com>
2019-03-14 17:20:15 +0000

    ECL 7.0.22-rc1 Release Candidate 1
    Signed-off-by: Richard Chapman <rchapman@hpccsystems.com>

--- version.cmake
+++ version.cmake
@@ -7,8 +7,8 @@ set ( HPCC_MAJOR 7 )
 
 set ( HPCC_MINOR 0 )
 set ( HPCC_POINT 7 )
-set ( HPCC_MATURITY "closedown" )
-set ( HPCC_SEQUENCE 0 )
+set ( HPCC_MATURITY "rc" )
+set ( HPCC_SEQUENCE 1 )
 ###
 IF ( ${HPCC_POINT} GREATER 65 )
   error ( "HPCC_POINT must be less than 66." )
