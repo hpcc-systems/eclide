53d767ff0beb7f4417d92c8114496a7a96eea07d
Richard Chapman <rchapman@hpccsystems.com>
2019-03-28 16:50:55 +0000

    ECL IDE 7.0.26-rc1 Release Candidate 1
    Signed-off-by: Richard Chapman <rchapman@hpccsystems.com>

--- version.cmake
+++ version.cmake
@@ -7,9 +7,9 @@ set ( HPCC_MAJOR 7 )
 
 
 set ( HPCC_MINOR 0 )
-set ( HPCC_POINT 25 )
-set ( HPCC_MATURITY "closedown" )
-set ( HPCC_SEQUENCE 0 )
+set ( HPCC_POINT 26 )
+set ( HPCC_MATURITY "rc" )
+set ( HPCC_SEQUENCE 1 )
 ###
 IF ( ${HPCC_POINT} GREATER 65 )
   error ( "HPCC_POINT must be less than 66." )
