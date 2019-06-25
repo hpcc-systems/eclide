hpcc-systems/eclide

commit 8f7b0ac3f0d867134293f14b1a39cf2e867edd82

Correct version.cmake for autotag process
Signed-off-by: Richard Chapman <rchapman@hpccsystems.com>

version.cmake
@@ -2,7 +2,7 @@
 ## Version Information
 ###
 set ( HPCC_NAME "ECL IDE" )
-set ( HPCC_PROJECT "community" )
+set ( HPCC_PROJECT "eclide" )
 set ( HPCC_MAJOR 7 )
 
 

commit 53a8ece42df6b1e9939a3cd3d3362fa5d30ca789

IDE-890 Package name is incorrect
Regression introduced with:  https://github.com/hpcc-systems/eclide/commit/8f7b0ac3f0d867134293f14b1a39cf2e867edd82

Signed-off-by: Gordon Smith <gordonjsmith@gmail.com>

CMakeLists.txt
@@ -109,6 +109,7 @@ ENDIF ()
 MESSAGE ("-- 64bit architecture is ${ARCH64BIT}")
 
 SET(projname ${HPCC_PROJECT})
+SET(audience ${HPCC_AUDIENCE})
 SET(majorver ${HPCC_MAJOR})
 SET(minorver ${HPCC_MINOR})
 SET(point ${HPCC_POINT})
@@ -131,7 +132,7 @@ INSTALL(DIRECTORY ${EXECUTABLE_OUTPUT_PATH}/Release/ CONFIGURATIONS Release DEST
 INSTALL(DIRECTORY ${EXECUTABLE_OUTPUT_PATH}/RelWithDebInfo/ CONFIGURATIONS RelWithDebInfo DESTINATION bin FILES_MATCHING PATTERN "*.dll")
 
 SET(CPACK_PACKAGE_VENDOR "HPCC Systems")
-SET(CPACK_PACKAGE_NAME "hpccsystems-eclide")
+SET(CPACK_PACKAGE_NAME "hpccsystems-${projname}")
 SET(CPACK_PACKAGE_VERSION_MAJOR ${majorver})
 SET(CPACK_PACKAGE_VERSION_MINOR ${minorver})
 SET(CPACK_PACKAGE_VERSION_PATCH ${point}${stagever})
@@ -142,7 +143,7 @@ SET(CPACK_PACKAGE_CONTACT "HPCCSystems <ossdevelopment@lexisnexis.com>")
 SET(CPACK_RESOURCE_FILE_LICENSE "${PROJECT_SOURCE_DIR}/LICENSE.txt")
 SET(CPACK_RESOURCE_FILE_README "${PROJECT_SOURCE_DIR}/CPackReadMe.txt")
 
-SET(CPACK_RPM_PACKAGE_VERSION "${projname}")
+SET(CPACK_RPM_PACKAGE_VERSION "${audience}")
 IF(${ARCH64BIT} EQUAL 1)
     SET(CPACK_RPM_PACKAGE_ARCHITECTURE "x86_64")
 ELSE(${ARCH64BIT} EQUAL 1)

version.cmake
@@ -3,6 +3,7 @@
 ###
 set ( HPCC_NAME "ECL IDE" )
 set ( HPCC_PROJECT "eclide" )
+set ( HPCC_AUDIENCE "community" )
 set ( HPCC_MAJOR 7 )
 set ( HPCC_MINOR 4 )
 set ( HPCC_POINT 0 )