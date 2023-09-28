68bd21108beeafcc855065d648dbe957fa95e3bc
Gavin Halliday <gavin.halliday@lexisnexis.com>
2023-09-28 21:24:28 +0100

    Split off 9.2.26
    Signed-off-by: Gavin Halliday <gavin.halliday@lexisnexis.com>

--- version.cmake
+++ version.cmake
@@ -6,7 +6,7 @@ set ( HPCC_PROJECT "eclide" )
 set ( HPCC_AUDIENCE "community" )
 set ( HPCC_MAJOR 9 )
 set ( HPCC_MINOR 2 )
-set ( HPCC_POINT 25 )
+set ( HPCC_POINT 27 )
 set ( HPCC_MATURITY "closedown" )
 set ( HPCC_SEQUENCE 0 )
 ###
