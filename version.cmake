b8eaa07b097a489ae10670309d55ab6a34253a25
Gordon Smith <GordonJSmith@gmail.com>
2024-08-02 11:49:32 +0100

    Split off 9.8.10
    Signed-off-by: Gordon Smith <GordonJSmith@gmail.com>

--- version.cmake
+++ version.cmake
@@ -6,7 +6,7 @@ set ( HPCC_PROJECT "eclide" )
 set ( HPCC_AUDIENCE "community" )
 set ( HPCC_MAJOR 9 )
 set ( HPCC_MINOR 8 )
-set ( HPCC_POINT 9 )
+set ( HPCC_POINT 11 )
 set ( HPCC_MATURITY "closedown" )
 set ( HPCC_SEQUENCE 0 )
 ###
