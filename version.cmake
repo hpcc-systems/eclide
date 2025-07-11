21527889346c204e4ce3ff31b7eb42fc25dcf617
Jake Smith <jake.smith@lexisnexisrisk.com>
2025-07-11 15:06:36 +0100

    Split off 9.6.124
    Signed-off-by: Jake Smith <jake.smith@lexisnexisrisk.com>

--- version.cmake
+++ version.cmake
@@ -6,7 +6,7 @@ set ( HPCC_PROJECT "eclide" )
 set ( HPCC_AUDIENCE "community" )
 set ( HPCC_MAJOR 9 )
 set ( HPCC_MINOR 6 )
-set ( HPCC_POINT 123 )
+set ( HPCC_POINT 125 )
 set ( HPCC_MATURITY "closedown" )
 set ( HPCC_SEQUENCE 0 )
 ###
