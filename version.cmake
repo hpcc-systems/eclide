4d29d3a14bf16842278f9d7c50828cb5842bd403
Jake Smith <jake.smith@lexisnexisrisk.com>
2026-01-09 16:25:37 +0000

    Split off 9.12.68
    Signed-off-by: Jake Smith <jake.smith@lexisnexisrisk.com>

--- version.cmake
+++ version.cmake
@@ -5,7 +5,7 @@ set ( HPCC_NAME "ECL IDE" )
 set ( HPCC_PROJECT "eclide" )
 set ( HPCC_MAJOR 9 )
 set ( HPCC_MINOR 12 )
-set ( HPCC_POINT 67 )
+set ( HPCC_POINT 69 )
 set ( HPCC_MATURITY "closedown" )
 set ( HPCC_SEQUENCE 0 )
 ###
